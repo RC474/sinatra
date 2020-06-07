4cde292c5aa68be9216fa2b2e236b827afd4712c
rameshchuwan19@gmail.com <ramesh@DESKTOP-7UE76BT.localdomain>
2020-06-06 19:08:14 -0500

    adds homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 
 class ApplicationController < Sinatra::Base 
   configure do 
+    enable :sessions
+    set :session_serect, 'randomserect'
+    set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get '/' do 
-    @new_hash = {
-      :name => "Ramesh",
-    }
-    erb :'homepage'
+    redirect to '/index.html'
   end
 
   get '/login' do 
